1cc7a6ddd10506a63725af87f7ed083a98e458b3
MaxChevalier <maxime.chevalier@ynov.com>
2024-03-26 18:16:23 +0100

    Refactor game initialization and bomb placement

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import sys
 from src.mainWindow import MainWindows
 from PySide6.QtWidgets import QApplication
 
-game_size = 5
-bombs = 10
-game = Game(game_size, bombs)
+game_size = {"X": 3, "Y": 3, "Z": 3, "W": 3}
+bombs = 5
 
 
 app = QApplication(sys.argv)
 
-main_wind = MainWindows(game, game_size, bombs)
+main_wind = MainWindows(game_size, bombs)
 main_wind.show()
 
 sys.exit(app.exec())
