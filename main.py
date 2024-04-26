f81b1ca8b485fafe98e4b1ed42fb20d10d70de2c
Maxime Chevalier <90835260+MaxChevalier@users.noreply.github.com>
2024-04-26 11:43:49 +0200

    Feat/show bombs (#7)
    * :bug: fix cell size
    
    * 💄 amélioration de l'affichage
    
    * 🐛 fix affichage all 0
    
    * 🐛 fix bomb placement
    
    * ✨ center windows and show all bombs when loose
    
    * ✨ generation en fonction du premier click
    
    * added CI/CD template (#5)
    
    * UI/amélioration de l'affichage (#6)
    
    * :bug: fix cell size
    
    * 💄 amélioration de l'affichage
    
    * ✨ generation en fonction du premier click
    
    * ✨ center windows and show all bombs when loose
    
    * ✨ generation en fonction du premier click
    
    * UI/amélioration de l'affichage (#6)
    
    * :bug: fix cell size
    
    * 💄 amélioration de l'affichage
    
    * ✨ generation en fonction du premier click
    
    * 🔥 remove debug print
    
    ---------
    
    Co-authored-by: tlsLizard <guillaume.noiseau@gmail.com>

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import sys
 from src.widget.mainWindow import MainWindows
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QScreen
 
 game_size = {"X": 3, "Y": 3, "Z": 3, "W": 3}
 bombs = 5
@@ -11,7 +12,12 @@ bombs = 5
 
 app = QApplication(sys.argv)
 
-main_wind = MainWindows(game_size, bombs)
-main_wind.show()
+main_window = MainWindows(game_size, bombs)
+main_window.show()
+
+center = QScreen.availableGeometry(QApplication.primaryScreen()).center()
+geo = main_window.frameGeometry()
+geo.moveCenter(center)
+main_window.move(geo.topLeft())
 
 sys.exit(app.exec())
